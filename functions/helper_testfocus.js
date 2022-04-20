9bb1fea50f1a99b556d6c6b8f1eca272bf036421
Tiernan Cahill <tjcahill@bu.edu>
2022-04-20 03:21:54 -0400

    Added switching cost calculation

--- functions/helper_testfocus.js
+++ functions/helper_testfocus.js
@@ -4,7 +4,12 @@ exports.buildFocusHeader = () => {
 
     const fields = ['ParticipantID', 'TestTime'];
 
-    const additionalFields = [];
+    const additionalFields = 
+    [
+        'AvgTimeR',
+        'AvgTimeNR',
+        'SwitchCost'
+    ];
 
     for (let i = 0; i < practiceRuns; i++) {
         fields.push(`PT${i + 1}_type`);
@@ -32,9 +37,11 @@ exports.buildFocusHeader = () => {
 exports.buildFocusData = (input) => {
     let str = "";
 
-    // Define aggregate variables here if necessary
-
     input.forEach(e => {
+        // Define aggregate variables here if necessary
+        let TotalTimeR = [];
+        let TotalTimeNR = [];
+
         str += e.id + ',';
         str += e.test_time + ',';
 
@@ -50,9 +57,22 @@ exports.buildFocusData = (input) => {
             str += subRes.numberOfBlueRectangles + ',';
             str += subRes.userChoice + ',';
             str += subRes.timeCost + ',';
+
+            if (subRes.isAnyRedTargetRotated) {
+                TotalTimeR.push(subRes.timeCost);
+            } else {
+                TotalTimeNR.push(subRes.timeCost);
+            }
         });
 
         // Calculate and add aggregate variables here if necessary
+        let AvgTimeR = calculateAvg(TotalTimeR);
+        let AvgTimeNR = calculateAvg(TotalTimeNR);
+        let SwitchCost = AvgTimeR - AvgTimeNR;
+
+        str += AvgTimeR + ',';
+        str += AvgTimeNR + ',';
+        str += SwitchCost + ',';
 
         str += '\n';
     });
@@ -61,3 +81,11 @@ exports.buildFocusData = (input) => {
 }
 
 // Helper functions for aggregation go here if necessary
+const calculateAvg = (input) => {
+    var total = 0;
+    for (var i = 0; i < input.length; i++) {
+        total += input[i];
+    }
+    var avg = total / input.length;
+    return avg;
+}
