dee1c27e6bf90ad457edd14e9d96b0ffed9ffd1d
Tiernan Cahill <tjcahill@bu.edu>
2022-05-13 01:14:44 -0400

    Create function variations triggered by Firestore

--- functions/index.js
+++ functions/index.js
@@ -67,6 +67,63 @@ exports.csvJsonReport_testfocus = functions.https.onRequest((request, response) 
         }).catch(err => console.log(err))
 });
 
+
+// The function variations below are intended to automatically trigger and automatically
+// re-export the user data each time a new record is created (which shouldn't happen more 
+// than a couple hundred times in production).
+exports.autoReport_multitask = functions.firestore.document('multitask/{doc}').onWrite((change, context) => {
+    const db = admin.firestore()
+    const bucketName = 'common-research.appspot.com';
+    const fileName = `reports/report_multitask.csv`;
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+
+    const storage = new Storage();
+    const myBucket = storage.bucket(bucketName);
+
+    db.collection('multitask')
+        .get()
+        .then(querySnapshot => {
+            const dataList = []
+            querySnapshot.forEach(doc => {
+                dataList.push(doc.data())
+            });
+            const header = buildMultitaskHeader();
+            const dataRow = buildMultitaskData(dataList);
+            return header.concat(dataRow);
+        }).then(csv => {
+            return fs.outputFile(tempFilePath, csv);
+        }).then(() => {
+            return myBucket.upload(tempFilePath, { destination: fileName })
+        }).catch(err => console.log(err))
+});
+
+exports.autoReport_testfocus = functions.firestore.document('testfocus/{doc}').onWrite((change, context) => {
+    const db = admin.firestore();
+    const bucketName = 'common-research.appspot.com';
+    const fileName = 'reports/report_testfocus.csv';
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+
+    const storage = new Storage();
+    const myBucket = storage.bucket(bucketName);
+
+    db.collection('testfocus')
+        .get()
+        .then(querySnapshot => {
+            const dataList = []
+            querySnapshot.forEach(doc => {
+                dataList.push(doc.data())
+            });
+            const header = buildFocusHeader(); // Modify this with new helper methods
+            const dataRow = buildFocusData(dataList); // And this
+            return header.concat(dataRow);
+        }).then(csv => {
+            return fs.outputFile(tempFilePath, csv);
+
+        }).then(() => {
+            return myBucket.upload(tempFilePath, { destination: fileName })
+        }).catch(err => console.log(err))
+});
+
 // exports.downloadReport = functions.https.onRequest((request, response) => {
 //     const bucketName = 'hci-multitask.appspot.com';
 //     const storage = new Storage();
